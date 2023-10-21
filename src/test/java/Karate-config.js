6728308cf6b91280eefc76a81b6ec1cd88224a5d
bikash1277 <bikashmishra1277@gmail.com>
2023-10-21 12:21:32 +0200

    commit v2

--- src/test/java/Karate-config.js
+++ src/test/java/Karate-config.js
@@ -1,21 +1,22 @@
-//function fn() {
-//var env = karate.env; // get system property 'karate.env' karate.log('karate.env system property was:', env);
-//if (!env) {
-//env = 'dev';
-//}
-//
-//var config = {
-//env: env,
-//myVarName: 'hello karate',
-//baseUrl: 'https://gorest.co.in',
-//tokenID: 'a1d248a1d1c837bd6e0c74c7fba0e36ea2cf4b38cf23fe6f6d6c23c3e1f9fb8d'
-//}
-//
-//if (env == 'dev') {
-//// customize
-//// e.g. config.foo = 'bar';
-//} else if (env == 'e2e') {
-//// customize
-//}
-//return config;
-//}
+function fn() {
+var env = karate.env;
+ // get system property 'karate.env' karate.log('karate.env system property was:', env);
+if (!env) {
+env = 'dev';
+}
+
+var config = {
+env: env,
+myVarName: 'hello karate',
+baseUrl: 'https://gorest.co.in',
+tokenID: 'a1d248a1d1c837bd6e0c74c7fba0e36ea2cf4b38cf23fe6f6d6c23c3e1f9fb8d'
+}
+
+if (env == 'dev') {
+// customize
+// e.g. config.foo = 'bar';
+} else if (env == 'e2e') {
+// customize
+}
+return config;
+}
